68cb089ea85d3dd40d23fcdcb3c69285f975412e
Damian Oliva <damian.oliva@hotmail.com>
2023-07-05 17:46:32 -0300

    cambie cos

--- index.js
+++ index.js
@@ -17,22 +17,7 @@ dotenv.config();
 
 conectarDB();
 
-// Configurar CORS
-const whitelist = [process.env.FRONTEND_URL];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      // Puede consultar la API
-      callback(null, true);
-    } else {
-      // No esta permitido su request
-      callback(new Error("Error de Cors"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Routing
 app.use("/api/usuarios", router);
